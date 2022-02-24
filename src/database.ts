d5c43afbbca7b59ee57d5f5e44daa5e7d3439ed7
Islam Sulaiman <ma.eslam@yahoo.com>
2022-02-24 14:03:30 +0300

    setting up prettier

--- src/database.ts
+++ src/database.ts
@@ -1,21 +1,17 @@
 import dotenv from 'dotenv';
-import {Pool} from 'pg';
+import { Pool } from 'pg';
 
 dotenv.config();
 
-const {
-    DATABASE_HOST,
-    POSTGRES_DB,
-    POSTGRES_USER,
-    POSTGRES_PASSWORD
-} = process.env
+const { DATABASE_HOST, POSTGRES_DB, POSTGRES_USER, POSTGRES_PASSWORD } =
+  process.env;
 
-const client = new Pool ({
-    host : DATABASE_HOST,
-    database : POSTGRES_DB,
-    user:POSTGRES_USER,
-    password : POSTGRES_PASSWORD
-})
+const client = new Pool({
+  host: DATABASE_HOST,
+  database: POSTGRES_DB,
+  user: POSTGRES_USER,
+  password: POSTGRES_PASSWORD,
+});
 
 console.log(DATABASE_HOST);
 export default client;
